e992d587c6bf53720f5da26ae7741d6bc5a7eaae
sergiuosvat <sergiuosvat@yahoo.com>
2024-08-07 10:59:42 +0300

    init template

---
+++ libs/services/src/services.module.ts
@@ -0,0 +1,16 @@
+import { Global, Module } from '@nestjs/common';
+import { DatabaseModule } from '@libs/database';
+import { DynamicModuleUtils } from '@libs/common';
+
+@Global()
+@Module({
+  imports: [
+    DatabaseModule,
+    DynamicModuleUtils.getCachingModule(),
+  ],
+  providers: [
+  ],
+  exports: [
+  ],
+})
+export class ServicesModule { }
